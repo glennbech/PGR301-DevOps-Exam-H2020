d250fb530e318d474c5378d9f07686a6ac7a46d4
Jonathan <Jonathan_Pusparajah@hotmail.com>
2020-11-21 13:05:51 +0100

    Added home page

---
+++ src/main/kotlin/no/devops/exam/HtmlController.kt
@@ -0,0 +1,17 @@
+package no.devops.exam
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HtmlController {
+
+    @GetMapping("/")
+    fun mainPage(model: Model): String {
+        model["title"] = "Monster Home page"
+        return "monster"
+    }
+
+}
--- src/main/kotlin/no/devops/exam/RestAPIController.kt
+++ src/main/kotlin/no/devops/exam/RestAPIController.kt
@@ -103,5 +103,4 @@ class RestAPIController(
         }
         return ResponseEntity.ok(monster.rarity.toList())
     }
-
 }
